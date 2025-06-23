a2dcb1a25ec0bacc59e4dc21fefa8b5d45d0916f
micromouse <eggeggak@vip.qq.com>
2025-06-23 18:24:52 +0800

    update web data pull by use selenium

--- WeibodataPullDemo.py
+++ WeibodataPullDemo.py
@@ -1,7 +1,10 @@
+from time import strftime
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from datetime import datetime, time
 import requests
 import json
 import time
@@ -25,9 +28,18 @@ def selenium_login_and_get_cookies():
     driver.quit()
     return get_cookies_dict(cookies)
 
-def fetch_comments(mid, cookies):
+def fetch_comments(mid: str, max_id: int, total_counter: int, cookies: dict) -> (int, int):
+    """
+    取得微博下帖子评论
+    :param mid: 帖子mid
+    :param max_id: 下一页id
+    :param total_counter: 总记录数
+    :param cookies: 登录用户cookies
+    :return: (max_id,total_counter)
+    """
     """使用 requests 抓取指定微博的评论"""
-    url = f"https://weibo.com/ajax/statuses/buildComments?flow=0&is_reload=1&id={mid}&is_show_bulletin=2&is_mix=0&count=20"
+    next_page = f"&max_id={max_id}" if max_id > 0 else ""
+    url = f"https://weibo.com/ajax/statuses/buildComments?flow=0&is_reload=1&id={mid}&is_show_bulletin=2&is_mix=0&count=20{next_page}"
 
     headers = {
         "User-Agent": "Mozilla/5.0",
@@ -38,21 +50,26 @@ def fetch_comments(mid, cookies):
     response = requests.get(url, headers=headers, cookies=cookies)
     if response.status_code != 200:
         print("请求失败，状态码：", response.status_code)
-        return
+        return 0, total_counter
 
     data = response.json()
-    print(json.dumps(data, indent=2, ensure_ascii=False))
     comments = data.get("data", [])
     print(f"共获取 {len(comments)} 条评论：")
     if not comments:
         print("当前页未返回任何评论，可能需要翻页")
+        return 0, total_counter
     else:
         try:
             for i, c in enumerate(comments):
-                print(c)
-                print(f"{i + 1}. {c['user']['screen_name']}: {c['text_raw']}")
+                created_ftime = (datetime
+                                 .strptime(c["created_at"], "%a %b %d %H:%M:%S %z %Y")
+                                 .strftime("%Y/%m/%d %H:%M:%S"))
+                print(f"{total_counter + i + 1}. {created_ftime}{c['user']['screen_name']}: {c['text_raw']}")
+            total_counter += len(comments)
         except Exception as e:
             print(f"获取评论时发生异常：{e}")
+
+        return data["max_id"], total_counter
 
 # ---------- 主程序入口 ----------
 if __name__ == "__main__":
@@ -61,6 +78,13 @@ if __name__ == "__main__":
 
     # 第二步：抓取微博评论（mid 是微博 ID，不是链接中的 UID）
     weibo_mid = "PxpqkDWRS"  # 替换成你想抓的微博 MID
-    fetch_comments(weibo_mid, cookies)
+    max_id = 0
+    total_counter = 0
+    while True:
+        max_id, total_counter = fetch_comments(weibo_mid, max_id, total_counter, cookies)
+        if max_id == 0:
+            break
 
+    print("按回车键退出")
+    input()
     sys.exit(0)
